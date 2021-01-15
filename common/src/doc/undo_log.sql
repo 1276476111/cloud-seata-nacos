cbed665e22efd1722782579d0f62326c69728857
1276476111@qq.com <1276476111@qq.com>
2021-01-15 20:10:18 +0800

    mvn插件。 项目包结构基本确定

---
+++ common/src/doc/undo_log.sql
@@ -0,0 +1,35 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : me-mysql8.0.16-3308
+ Source Server Type    : MySQL
+ Source Server Version : 80016
+ Source Host           : localhost:3308
+ Source Schema         : my-seata-stock
+
+ Target Server Type    : MySQL
+ Target Server Version : 80016
+ File Encoding         : 65001
+
+ Date: 15/01/2021 20:09:13
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for undo_log
+-- ----------------------------
+DROP TABLE IF EXISTS `undo_log`;
+CREATE TABLE `undo_log`  (
+  `branch_id` bigint(20) NOT NULL COMMENT 'branch transaction id',
+  `xid` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'global transaction id',
+  `context` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'undo_log context,such as serialization',
+  `rollback_info` longblob NOT NULL COMMENT 'rollback info',
+  `log_status` int(11) NOT NULL COMMENT '0:normal status,1:defense status',
+  `log_created` datetime NOT NULL COMMENT 'create datetime',
+  `log_modified` datetime NOT NULL COMMENT 'modify datetime',
+  UNIQUE INDEX `ux_undo_log`(`xid`, `branch_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = 'AT transaction mode undo table' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- common/src/main/java/com/qsn/common/CommonApplication.java
+++ common/src/main/java/com/qsn/common/CommonApplication.java
@@ -3,6 +3,12 @@ package com.qsn.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 公共类
+ *
+ * @author qiusn
+ * @date 2020-01-15
+ */
 @SpringBootApplication
 public class CommonApplication {
 
