c2c47b59322c6265658a7cac87dd94b961560325
1276476111@qq.com <1276476111@qq.com>
2021-01-26 20:36:07 +0800

    common新增测试类 与 配置类

---
+++ gateway/src/main/java/com/qsn/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.qsn.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关
+ *
+ * @author qiusn
+ * @date 2021-01-26
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
